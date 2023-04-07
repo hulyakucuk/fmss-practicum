c9d8fa2d4c1b2964a5d4a8bd63895bae75dc9bd9
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-07 17:56:40 +0300

    add filter

--- week_02/todo-list/src/App.js
+++ week_02/todo-list/src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{ useState } from 'react';
 import { useEffect } from 'react';
-import { Button,FormControl,Form } from 'react-bootstrap';
-
-import { ReactComponent as Edit } from './assets/edit.svg';
+import { Button,FormControl } from 'react-bootstrap';
 import { ReactComponent as Trash } from './assets/trash.svg';
-import { ReactComponent as Save } from './assets/save.svg';
+
+// uuid:To create a random unique id
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -14,56 +14,81 @@ function App() {
   const[todoList,setTodoList]=useState([])
 
   const[todo,setTodo]=useState("");
+  
+  const[filter,setFilter]=useState("all");
+
 
   const addTodo=()=>{
-    setTodoList((prevTodoList)=>[...prevTodoList,todo])
+    setTodoList((prevTodoList)=>[...prevTodoList,{id:uuidv4(), todo:todo, isCompleted:false}])
     setTodo("")
   }
+  const completedTodo=(id)=>{
+   setTodoList(prevTodoList=>prevTodoList.map(todoItem=> todoItem.id===id
+    ?{...todoItem,isCompleted: !todoItem.isCompleted}:todoItem))
+  }
 
-  return (
 
+  const deleteTodo=(id)=>{
+    setTodoList(prevTodoList => prevTodoList.filter(todoItem=>todoItem.id !==id ))
+
+  }
+
+  let filteredList = [];
+  if (filter === "completed") {
+    filteredList = todoList.filter((todoItem) => todoItem.isCompleted);
+  } else if (filter === "active") {
+    filteredList = todoList.filter((todoItem) => !todoItem.isCompleted);
+  } else {
+
+    filteredList = todoList;
+  }
+  
+
+    useEffect(()=>{
+      console.log(todoList)
+    },[todoList])
+
+
+      return (
+
+        
       <div className="container ps-5 pe-5 d-flex flex-column w-50 mt-5">
-    <div className='d-flex mt-5'>
-    <FormControl
-          className='w-70'
-          placeholder="What needs to be done?"
-          value={todo}
-          onChange={(e)=>setTodo(e.target.value)}
-        />
+      <div className='wrap d-flex mt-5'>
+      <FormControl
+            className='w-70'
+            placeholder="What needs to be done?"
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)}
+          />
         <Button
         onClick={()=>{addTodo()}}
-         className='ms-3 bg-success border-0'>Add Todo</Button>
+         className='btn ms-3 bg-success border-0'>Add Todo</Button>
       
     </div>
-    <div className=' mt-3 w-80 '>
-      {
-        todoList.map(
-          (todoItem,index) =><div className='d-flex mt-2 justify-content-between' key={index}>
-            <div className='d-flex'>
-             <Form.Check 
-             type="checkbox"
-             className='me-2'
-            />
-            <label>
-            {todoItem}
+      
+        <div className='d-flex flex-row mt-3' >
+        <button  className='title' onClick={() => setFilter("all")}>All</button>
+        <button className='title' onClick={() => setFilter("active")}>Active</button>
+        <button className='title' onClick={() => setFilter("completed")}>Completed</button>
+        </div>
+
+     <div className=' w-80 mt-2'>
+      {filteredList.map(
+        (todoItem) =><div className='d-flex mt-2 justify-content-between' key={todoItem.id}>
+          <div className='d-flex'>
+            <label className={ `${todoItem.isCompleted ? "text-decoration-line-through" : ""}` } onClick={()=>{completedTodo(todoItem.id)}} >
+            {todoItem.todo}<hr></hr>
             </label>
             </div>
           <div className=''>
           <button className='border-0 bg-transparent'>
-          <Trash className='me-1 icon'  width={20} height={20} ></Trash>
-          <Edit className='me-1 icon' width={20} height={20}></Edit>
-          <Save className='icon' width={20} height={20}></Save>
+              <Trash onClick={()=>deleteTodo(todoItem.id)} className=' icon'  width={20} height={20} ></Trash>
           </button>
           </div>
-    
           </div>
-        )
-      }
+        )}
     </div>
-      </div>
-     
-       
-
+    </div>
   );
 }
 
