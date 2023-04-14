b1e55bda81f76678d3a088e0004c3f0360e1f0af
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-14 20:35:15 +0300

    add API key to the .env and  add .env to the gitignore file to ensure it is not uploaded to the repository

---
+++ week_03/weather-app/src/Context/WeatherContext.js
@@ -0,0 +1,2 @@
+const REACT_APP_WEATHER_API_KEY=process.env.REACT_APP_WEATHER_API_KEY;
+const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${REACT_APP_WEATHER_API_KEY}`
