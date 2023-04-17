e0ca48e7346c8f5a30249cda975a2f41de0c114f
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-17 10:03:45 +0300

    feat: Capture city input changes in state variable
    This commit updates the 'handleCityChange' function to set the 'city' state variable using the 'useState' hook. The function captures the changes made by the user in the input field and updates the 'city' variable accordingly.

--- week_03/weather-forecast/src/context/Context.js
+++ week_03/weather-forecast/src/context/Context.js
@@ -1,13 +1,21 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const Context =createContext();
 // context elemanı olusturuldu 
 
 const ContextProvider=({children})=>{
+    
+    const [city,setCity] =useState("");
+    // saves the user's input as a variable
 
 
+    const handleCityChange=(event)=> {
+        setCity(event.target.value);
+        console.log("gitrilne değer",event.target.value);
+    }
+    // this function captures the changes made by the user in the input field
 
-return(
+    return(
     <Context.Provider>
         {children}
     </Context.Provider>
