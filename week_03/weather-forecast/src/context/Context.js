a6fb5f9b24d72a1298bf5c3b78f7bb90cc9ec70b
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-17 09:10:15 +0300

    add Context to manage state globally

---
+++ week_03/weather-forecast/src/context/Context.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+
+export const Context =useContext();
+// context elemanı olusturuldu 
+
+const ContextProvider=()=>{
+
+};
+
+return(
+    <ContextProvider>
+
+    </ContextProvider>
+)
+
+export default ContextProvider;
