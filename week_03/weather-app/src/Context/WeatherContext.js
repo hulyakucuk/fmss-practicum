f847bf3588cd29ed4edcd312cbba08b076a72e64
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-17 16:28:49 +0300

    add week-03 weather app project

--- week_03/weather-app/src/Context/WeatherContext.js
+++
@@ -1,2 +0,0 @@
-const REACT_APP_WEATHER_API_KEY=process.env.REACT_APP_WEATHER_API_KEY;
-const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${REACT_APP_WEATHER_API_KEY}`
--- week_03/weather-appp/src/Context/WeatherContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const WeatherContext=createContext();
-
-export const WeatherProvider=()=>{
-
-};
-
-export default WeatherContext;
