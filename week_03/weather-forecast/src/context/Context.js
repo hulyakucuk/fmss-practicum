5ba55bc9326a700c0927bfd6d454f352659af231
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-17 10:36:33 +0300

    values object is stored to be passed down to child components through the value prop of the Provider

--- week_03/weather-forecast/src/context/Context.js
+++ week_03/weather-forecast/src/context/Context.js
@@ -15,13 +15,15 @@ const ContextProvider=({children})=>{
     // This function is triggered when the user submits the form and sends a request to the API using the fetch() function
         
         event.preventDefault();
-    // preventDefault() is used to prevent the default behavior of the form so that the data does not disappear when the page is reloaded.
+        // preventDefault() is used to prevent the default behavior of the form so that the data does not disappear when the page is reloaded.
        
         const response=await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`);
         if(!response.ok){
             serWeatherData(null);
+        //  If there is an error with the response from the API, the weatherData state variable is set to null 
             return;
         }
+
         const data = await response.json();
         serWeatherData(data);
         console.log("weather data:",data);
@@ -32,10 +34,19 @@ const ContextProvider=({children})=>{
     }
     // this function captures the changes made by the user in the input field
 
+    const values={
+        city,
+        handleCityChange,
+        handleSubmit,
+        weatherData
+    };
+    // values are stored in an object called values to be passed to a child component.
+    
     return(
-    <Context.Provider>
+    <Context.Provider value={{values}}>
         {children}
     </Context.Provider>
 )
 };
 export default ContextProvider;
+
