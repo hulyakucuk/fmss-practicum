04e506fd8afc445dd5a3158b8e60860f72eec178
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-26 17:33:54 +0300

    add Card component and card style features

---
+++ book_search_app/src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+      <div className='card'>
+        <img />
+        <div className='bottom'>
+            <h3 className='title'>React.js</h3>
+            <p className='amount'>&8377;3290</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card;
--- book_search_app/src/components/Header.js
+++ book_search_app/src/components/Header.js
@@ -1,29 +1,35 @@
-import React from 'react'
-import {FaSearch} from "react-icons/fa";
-import sample from "./sample.avif"
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import Card from "./Card";
 
 const Header = () => {
   return (
     <>
-    <div className='header'>
-        <div className='row1'>
-            <h1>A reader lives a thousand<br/>lives before he dies . . . <br/>
-            The man who never reads <br/>lives only one.</h1>
+      <div className="header">
+        <div className="row1">
+          <h1>
+            A reader lives a thousand
+            <br />
+            lives before he dies . . . <br />
+            The man who never reads <br />
+            lives only one.
+          </h1>
         </div>
-        <div className='row2'>
-        <h2>Search Your Book</h2>
-        <div className='search'>
-            <input type='text'
-            placeholder='Enter your book name' />
-            <button>    
-            <FaSearch className='text-purple' size = {32} />
+        <div className="row2">
+          <h2>Search Your Book</h2>
+          <div className="search">
+            <input type="text" placeholder="Enter your book name" />
+            <button>
+              <FaSearch className="text-purple" size={32} />
             </button>
+          </div>
         </div>
-        </div>
-    </div>
-      
+      </div>
+      <div className="container">
+        <Card />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
