1580b3553f037404cff93d1f99e711ee3d3b0baf
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-26 17:47:51 +0300

    add fetchData function for the get data

--- book_search_app/src/components/Context.js
+++ book_search_app/src/components/Context.js
@@ -11,13 +11,21 @@ const ContextProvider=({children})=>{
    const [bookData,setData]=useState([]);
     // save data retrieved from the API
 
-
+    const fetchData=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBqVdHeiWGWGcptZWf6wjzRllE0MdZabhk'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    };
 
     const values={
         search,
         setSearch,
         bookData,
         setData,
+        fetchData
 
     }
  // values are stored in an object called values to be passed to a child component.
