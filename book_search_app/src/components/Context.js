b95b6b1ebaec706fd97329078e741ae9295b8781
hulyakucuk <0hulyakucuk@gmail.com>
2023-04-26 17:44:23 +0300

    add search state to save the user s input as a variable and bookData state to save data retrieved from the API

--- book_search_app/src/components/Context.js
+++ book_search_app/src/components/Context.js
@@ -1,9 +1,31 @@
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
 
 export const Context=createContext();
 
 const ContextProvider=({children})=>{
 
-}
+    const [search,setSearch]=useState("");
+   // saves the user's input as a variable
+
+   const [bookData,setData]=useState([]);
+    // save data retrieved from the API
+
+
+
+    const values={
+        search,
+        setSearch,
+        bookData,
+        setData,
+
+    }
+ // values are stored in an object called values to be passed to a child component.
+
+    return (
+        <Context.Provider value={values}>
+        {children}
+    </Context.Provider>
+    )
+};
 
 export default ContextProvider;
